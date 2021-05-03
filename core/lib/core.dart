6022f42d7839dfd20a047e16c0f4756d1ef51628
Regis Kian <regisksc@gmail.com>
2021-05-03 04:45:04 -0300

    added roles to core

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -4,3 +4,5 @@ export './factories/factories.dart';
 export './adapters/adapters.dart';
 export './data/data.dart';
 export './decorators/decorators.dart';
+export './resources/resources.dart';
+export './error/error.dart';
