ddd13e110effc53de2661bce346d7624623a8bc8
Regis Kian <regisksc@gmail.com>
2021-05-03 05:03:55 -0300

    completed a module

---
+++ module_x/lib/data/repositories/concrete_example_repository.dart
@@ -0,0 +1,8 @@
+import 'package:module_x/domain/repositories/example_repository.dart';
+
+class ConcreteExampleRepository implements ExampleRepository {
+  @override
+  void exampleMethod() {
+    // TODO: implement exampleMethod
+  }
+}
