fcc475cb7691df765929e816b3b8601b8e8f7ed1
Regis Kian <regisksc@gmail.com>
2021-05-04 15:29:28 -0300

    added checkout module

---
+++ checkout/lib/data/repositories/concrete_example_repository.dart
@@ -0,0 +1,8 @@
+import '../../domain/domain.dart';
+
+class ConcreteExampleRepository implements ExampleRepository {
+  @override
+  void exampleMethod() {
+    // TODO: implement exampleMethod
+  }
+}
