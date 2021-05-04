244cc6dd04a9cccfbca52dfe9b844f07589fedaa
Regis Kian <regisksc@gmail.com>
2021-05-04 17:50:39 -0300

    added features folder under core

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -3,6 +3,5 @@ library core;
 export './factories/factories.dart';
 export './adapters/adapters.dart';
 export './data/data.dart';
-export './decorators/decorators.dart';
 export './resources/resources.dart';
 export './error/error.dart';
---
+++ core/lib/features/fedatures.dart
@@ -0,0 +1,1 @@
+export './feature1/feature1.dart';
