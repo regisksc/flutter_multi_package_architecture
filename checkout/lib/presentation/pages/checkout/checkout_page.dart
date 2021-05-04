57e19b827006305c0f2e903df984b412b43a5c79
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-04 17:25:12 -0300

    run checkout page

---
+++ checkout/lib/presentation/pages/checkout/checkout_page.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'checkout_controller.dart';
+
+class CheckoutPage extends GetView<CheckoutController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CheckoutPage'),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ checkout/lib/presentation/presentation.dart
@@ -0,0 +1,1 @@
+export './pages/checkout/checkout_page.dart';
