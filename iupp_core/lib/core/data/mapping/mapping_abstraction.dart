bae1d14aa648a89051b95f3a09e550ce98b2ab8e
Regis Kian <regisksc@gmail.com>
2021-05-19 21:26:39 -0300

    Update mapping_abstraction.dart

--- iupp_core/lib/core/data/mapping/mapping_abstraction.dart
+++ iupp_core/lib/core/data/mapping/mapping_abstraction.dart
@@ -1,4 +1,9 @@
-abstract class Mapper {
+abstract class Mapper implements MapperFromJson, MapperToJson {}
+
+abstract class MapperFromJson {
   Mapper fromJson(Map<String, dynamic> json);
+}
+
+abstract class MapperToJson {
   Map<String, dynamic> get toJson;
 }
