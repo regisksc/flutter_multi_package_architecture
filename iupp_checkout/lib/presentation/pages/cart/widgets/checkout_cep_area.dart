d3babcc4f2c56f5e76cbb109babeab90966a1993
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-21 14:45:29 -0300

    fix imports and packags pos merge

--- iupp_checkout/lib/presentation/pages/cart/widgets/checkout_cep_area.dart
+++ iupp_checkout/lib/presentation/pages/cart/widgets/checkout_cep_area.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:iupp_components/iupp_icons.dart';
+import 'package:iupp_components/components/components.dart';
 import 'package:iupp_core/core/utils/formatters.dart';
 
 class CheckoutCepArea extends StatelessWidget {
