eaf9e4acc5f1c6beffdde3da2cec07692c4a1e95
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-21 16:17:11 -0300

    refactor cart empty

--- iupp_checkout/lib/presentation/pages/cart/widgets/checkout_empty_cart.dart
+++ iupp_checkout/lib/presentation/pages/cart/widgets/checkout_empty_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iupp_checkout/presentation/widgets/widgets.dart';
+import 'package:iupp_components/components/molecules/cards/cards.dart';
 
 import 'package:iupp_core/core.dart';
 
@@ -10,30 +11,25 @@ class CheckoutEmptyCart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          height: 106,
+        const SizedBox(
           width: double.maxFinite,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Seu carrinho está vazio",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  "Vá para o início e veja outras ofertas",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                ),
-              ],
-            ),
+          child: IuppCard(
+            children: [
+              Text(
+                "Seu carrinho está vazio",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Vá para o início e veja outras ofertas",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff494C57)),
+              ),
+            ],
           ),
         ),
         IuppCheckoutButtonNavigate(
