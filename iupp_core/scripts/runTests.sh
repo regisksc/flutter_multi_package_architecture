346207c3a8bad23515529682042f3a86a7a6c85f
Rodrigo Bastos <rodrigobastosv@gmail.com>
2021-05-21 22:29:12 -0300

    removing some stuff from coverage

--- iupp_core/scripts/runTests.sh
+++ iupp_core/scripts/runTests.sh
@@ -6,6 +6,6 @@ flutter pub global activate remove_from_coverage
 echo '############################### Running tests ################################'
 flutter test --coverage
 echo '############################### Removing generated files #####################'
-flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*'
+flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*' -r 'lib/core/transition*' -r 'navigator_service.dart'
 echo '############################### Generating coverage ##########################'
 genhtml coverage/lcov.info -o coverage/html
