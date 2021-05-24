9f3c8a24f9bed8d9e730e5520c9b8701ac392f98
Rodrigo Bastos <rodrigobastosv@gmail.com>
2021-05-24 09:55:29 -0300

    adding logging to core

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/widgets.dart';
+import 'package:iupp_core/core.dart';
+import 'package:iupp_core/init_core.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'main/app_widget.dart';
 
-void main() {
+Future<void> main() async {
+  await initCore();
   setPathUrlStrategy();
   runApp(IuppApp());
 }
