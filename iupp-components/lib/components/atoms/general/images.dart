d4001bd2e7fc54058a3686f552d810f201368301
Rodrigo Bastos <rodrigobastosv@gmail.com>
2021-05-25 20:57:01 -0300

    adding tests to iupp images

--- iupp-components/lib/components/atoms/general/images.dart
+++ iupp-components/lib/components/atoms/general/images.dart
@@ -3,16 +3,20 @@ import 'package:flutter/widgets.dart';
 class IuppImage extends Image {
   IuppImage.asset(
     String name, {
+    Key? key,
     BoxFit? fit,
   }) : super(
+          key: key,
           image: AssetImage('assets/images/$name', package: 'iupp_components'),
           fit: fit,
         );
 
   IuppImage.network(
     String url, {
+    Key? key,
     BoxFit? fit,
   }) : super(
+          key: key,
           image: NetworkImage(url),
           fit: fit,
         );
