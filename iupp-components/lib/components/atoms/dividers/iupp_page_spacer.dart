4621d91d202cfb9ae12c196e0a0fbdf0522cd37c
Regis Kian <regisksc@gmail.com>
2021-05-26 17:09:53 -0300

    Update iupp_page_spacer.dart

--- iupp-components/lib/components/atoms/dividers/iupp_page_spacer.dart
+++ iupp-components/lib/components/atoms/dividers/iupp_page_spacer.dart
@@ -1,46 +1,18 @@
 import 'package:iupp_core/core.dart';
 
+import '../../components.dart';
+
 class IuppPageSpacer extends StatelessWidget {
   const IuppPageSpacer({
     Key? key,
-    this.top,
-    this.bottom,
-    this.right,
-    this.left,
-    this.child,
-    this.preferPercentage = true,
-    this.percentageValue,
-    this.staticValue,
-  })  : assert(preferPercentage == true && staticValue == null || preferPercentage == false && staticValue != null),
-        super(key: key);
-
-  final bool preferPercentage;
-  final double? staticValue;
-  final bool? top;
-  final bool? bottom;
-  final bool? right;
-  final bool? left;
-  final double? percentageValue;
-  final Widget? child;
-
-  bool get isAllSides => top == null && bottom == null && right == null && left == null;
+    this.height,
+    this.width,
+  }) : super(key: key);
+  final double? height;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
-    final percentage = (percentageValue ?? 3) / 100;
-
-    final size = MediaQuery.of(context).size;
-    final defaultHeightInPercentage = preferPercentage ? size.height * percentage : staticValue!;
-    final defaultWidthInPercentage = preferPercentage ? size.width * percentage : staticValue!;
-
-    return Padding(
-      padding: EdgeInsets.only(
-        top: top != null || isAllSides ? defaultHeightInPercentage : 0,
-        bottom: bottom != null || isAllSides ? defaultHeightInPercentage : 0,
-        left: left != null || isAllSides ? defaultWidthInPercentage : 0,
-        right: right != null || isAllSides ? defaultWidthInPercentage : 0,
-      ),
-      child: child ?? Container(),
-    );
+    return SizedBox(height: height ?? SizeConstants.pageSidePadding, width: width);
   }
 }
