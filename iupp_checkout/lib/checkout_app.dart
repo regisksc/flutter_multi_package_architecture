b7e3408ed5f2f21093160189174fa6a5adca5710
Regis Kian <regisksc@gmail.com>
2021-05-28 13:37:55 -0300

    added route

--- iupp_checkout/lib/checkout_app.dart
+++ iupp_checkout/lib/checkout_app.dart
@@ -15,10 +15,9 @@ class CheckoutApp extends MicroApp {
         ADDRESS_LIST_PAGE: (_, args) => const AddressesListPage(),
         DELIVERY_OPTIONS_PAGE: (_, args) => const DeliveryOptionsPage(),
         CART_PAGE: (_, args) => const CartPage(),
-        PAYMENT_METHOD_SELECTION_PAGE: (_, args) =>
-            const PaymentMethodSelectionPage(),
-        INSTALLMENT_SELECTION_PAGE: (_, args) =>
-            InstallmentSelectionPage(cardTitle: args!.data['card'] as String),
+        PAYMENT_METHOD_SELECTION_PAGE: (_, args) => const PaymentMethodSelectionPage(),
+        INSTALLMENT_SELECTION_PAGE: (_, args) => InstallmentSelectionPage(cardTitle: args!.data['card'] as String),
+        ORDER_REVIEW_PAGE: (_, args) => const OrderReviewPage(),
         SUCCESSFUL_ORDER_PAGE: (_, args) => SuccessfulOrderPage(),
       };
 }
